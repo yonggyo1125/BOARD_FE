0d92199159abf5828238ac6069a73f68cd36ee7c
yonggyo1125 <yonggyo00@kakao.com>
2024-02-16 15:51:34 +0900

    .'

--- src/commons/components/TitleStyle.js
+++ src/commons/components/TitleStyle.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+import { fontSize } from '../../styles/size';
+
+const { extraBig } = fontSize;
+
+export const MainTitle = styled.h1`
+  font-size: ${extraBig}rem;
+  font-weight: 700;
+  line-height: 1;
+  padding: 0 15px 20px;
+  border-bottom: 2px solid #000;
+  margin-bottom: 35px;
+`;
--- src/member/pages/JoinPage.js
+++ src/member/pages/JoinPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import JoinContainer from '../containers/JoinContainer';
+import { MainTitle } from '../../commons/components/TitleStyle';
 
 const JoinPage = () => {
   const { t } = useTranslation();
@@ -11,6 +12,7 @@ const JoinPage = () => {
       <Helmet>
         <title>{t('회원가입')}</title>
       </Helmet>
+      <MainTitle>{t('회원가입')}</MainTitle>
       <JoinContainer />
     </>
   );
