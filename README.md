cbb4ea69fb0f220a6721006a2e336073b02c9335
yonggyo1125 <yonggyo00@kakao.com>
2024-02-16 14:26:16 +0900

    .

--- README.md
+++ README.md
@@ -32,13 +32,14 @@
 - axios : ajax
 - i18next, react-i18next : 메세지, 다국어 처리
 - react-helmet-async : head 태그 내부 구성을 변경할때
+- react-icons : 리액트에서 제공하는 아이콘 모음
 
 ```
-yarn add react-router-dom sass styled-components classnames @loadable/component immer axios i18next react-i18next
+yarn add react-router-dom sass styled-components classnames @loadable/component immer axios i18next react-i18next react-icons
 ```
 
 ```
-npm i react-router-dom sass styled-components classnames @loadable/component immer axios i18next react-i18next
+npm i react-router-dom sass styled-components classnames @loadable/component immer axios i18next react-i18next react-icons
 ```
 
 # 초기 구성 - src/index.js
--- src/outlines/front/Header.js
+++ src/outlines/front/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { FaUserPlus } from 'react-icons/fa';
+import { FiLogIn, FiSearch } from 'react-icons/fi';
+import logo from '../../images/logo.png';
 
 const HeaderBox = styled.header`
   background: #fff;
@@ -7,9 +12,30 @@ const HeaderBox = styled.header`
 `;
 
 const Header = () => {
+  const { t } = useTranslation();
   return (
     <HeaderBox>
-      <div className="layout-width"></div>
+      <div className="layout-width">
+        <Link to="/" className="logo">
+          <img src={logo} alt="logo" />
+        </Link>
+
+        <form>
+          <input type="text" name="skey" placeholder={t('검색어_입력')} />
+          <button type="submit">
+            <FiSearch />
+          </button>
+        </form>
+
+        <div class="links">
+          <Link to="/member/login">
+            <FiLogIn />
+          </Link>
+          <Link to="/member/join">
+            <FaUserPlus />
+          </Link>
+        </div>
+      </div>
     </HeaderBox>
   );
 };
