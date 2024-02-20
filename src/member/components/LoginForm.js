d4de94fe5572534eb3658f9be64d3a7595688df0
yonggyo1125 <yonggyo00@kakao.com>
2024-02-20 09:32:58 +0900

    .

--- src/member/components/LoginForm.js
+++ src/member/components/LoginForm.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { FiLock, FiKey, FiUserPlus } from 'react-icons/fi';
 import { InputText } from '../../commons/components/InputBoxStyle';
 import { MediumButton } from '../../commons/components/ButtonStyle';
+import { fontSize } from '../../styles/size';
+const { medium } = fontSize;
 
 const LoginBox = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: fixed;
+  position: absolute;
   width: 100%;
   height: calc(100% - 315px);
   left: 0;
-  top: 315px;
+  top: 211px;
+
   .inner {
     width: 350px;
+
+    h1 {
+      text-align: center;
+      color: ${medium};
+      margin-bottom: 10px;
+    }
+
+    .links {
+      display: flex;
+      margin-top: 10px;
+      padding: 15px 0;
+      border: 1px solid #d5d5d5;
+      border-left: 0;
+      border-right: 0;
+
+      a {
+        width: 0;
+        flex-grow: 1;
+        text-align: center;
+      }
+    }
+
+    button {
+      margin-top: 10px;
+    }
   }
 `;
 
@@ -30,9 +60,24 @@ const LoginForm = () => {
           name="password"
           placeholder={t('비밀번호')}
         />
+
         <MediumButton type="submit" bcolor="primary" fcolor="#fff">
           {t('로그인')}
         </MediumButton>
+
+        <div className="links">
+          <Link to="/member/find_id">
+            <FiLock /> {t('아이디_찾기')}
+          </Link>
+
+          <Link to="/member/find_pw">
+            <FiKey /> {t('비밀번호_찾기')}
+          </Link>
+
+          <Link to="/member/join">
+            <FiUserPlus /> {t('회원가입')}
+          </Link>
+        </div>
       </div>
     </LoginBox>
   );
