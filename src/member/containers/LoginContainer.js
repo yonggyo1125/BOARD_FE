03cc87d3e5f05c15148c37f76f57f7d2a847b79e
yonggyo1125 <yonggyo00@kakao.com>
2024-02-20 09:41:51 +0900

    .

--- src/member/containers/LoginContainer.js
+++ src/member/containers/LoginContainer.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 import LoginForm from '../components/LoginForm';
+import { produce } from 'immer';
 
 const LoginContainer = () => {
-  return <LoginForm />;
+  const [form, setForm] = useState({});
+
+  const onChange = useCallback(
+    (e) =>
+      setForm(
+        produce((draft) => (draft[e.target.name] = e.target.value.trim())),
+      ),
+    [],
+  );
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  return <LoginForm onChange={onChange} onSubmit={onSubmit} form={form} />;
 };
 
 export default React.memo(LoginContainer);
