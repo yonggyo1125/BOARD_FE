0d3fe02c01b32661f2ec6af88d713c479abd3e61
yonggyo1125 <yonggyo00@kakao.com>
2024-02-26 09:42:30 +0900

    .

---
+++ src/commons/libs/apiRequest.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default function apiRequest(url, method = 'GET', data, headers) {
+  // /member/join -> http://localhost:3001/api/v1/member/join
+  // https://주소/api/....
+  if (!/^http[s]?:/i.test(url)) {
+    url = process.env.REACT_APP_API_URL + url;
+  }
+
+  // GET -> ?키=값&키=값
+  method = method.toUpperCase();
+  if (method === 'GET' && data) {
+    const params = new URLSearchParams(data);
+    url += '?' + params.toString();
+    data = null;
+  }
+  // 검증 실패시 400
+  return axios({
+    method,
+    url,
+    data,
+    headers,
+    validityState: (state) => state < 500,
+  });
+}
