a8c86bbae2f7e29a83d64427e1657966516e17f9
yonggyo1125 <yonggyo00@kakao.com>
2024-02-27 11:46:52 +0900

    .

--- src/admin/menus.js
+++ src/admin/menus.js
@@ -5,10 +5,18 @@ const menus = {
     { url: '/admin/member/add', name: '회원등록' },
   ],
   board: [
-    { url: '/admin/board', name: '게시판 목록' },
-    { url: '/admin/board/add', name: '게시판 등록' },
-    { url: '/admin/board/list', name: '게시글 관리' },
+    { url: '/admin/board', name: '게시판_목록' },
+    { url: '/admin/board/add', name: '게시판_등록' },
+    { url: '/admin/board/list', name: '게시글_관리' },
   ],
+};
+
+export const getSubMenus = (menuCode) => {
+  if (!menuCode || !menuCode.trim()) {
+    menuCode = window.location.pathname.split('/')[2];
+
+    return menuCode ? menus[menuCode] : [];
+  }
 };
 
 export default menus;
--- src/outlines/admin/SubMenu.js
+++ src/outlines/admin/SubMenu.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { getSubMenus } from '../../admin/menus';
+import classNames from 'classnames';
+
+const NavBox = styled.nav``;
 
 const SubMenu = () => {
-  return <h1>서브메뉴</h1>;
+  const [items, setItems] = useState(null);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    setItems(getSubMenus());
+  }, []);
+
+  return (
+    items &&
+    items.length > 0 && (
+      <NavBox>
+        {items.map(({ url, name }) => (
+          <NavLink
+            key={url.replace(/\//g, '_')}
+            to={url}
+            className={({ isActive }) => classNames({ on: isActive })}
+          >
+            {t(name)}
+          </NavLink>
+        ))}
+      </NavBox>
+    )
+  );
 };
 
 export default React.memo(SubMenu);
