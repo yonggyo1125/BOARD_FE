74f8797787cd7d68781f55b8bea927fe5d504a50
yonggyo1125 <yonggyo00@kakao.com>
2024-03-04 16:47:40 +0900

    .

--- src/admin/board/components/BoardConfigForm.js
+++ src/admin/board/components/BoardConfigForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { IoIosRadioButtonOff, IoIosRadioButtonOn } from "react-icons/io";
+import { IoIosRadioButtonOff, IoIosRadioButtonOn } from 'react-icons/io';
 import { TableCols } from '../../../commons/components/admin/TableStyle';
 import { InputText } from '../../../commons/components/InputBoxStyle';
+import { SubTitle } from '../../../commons/components/TitleStyle';
 
 const FormBox = styled.form``;
 
@@ -12,6 +13,7 @@ const BoardConfigForm = () => {
 
   return (
     <FormBox>
+      <SubTitle>{t('일반설정')}</SubTitle>
       <TableCols>
         <tr>
           <th>{t('게시판_아이디')}</th>
@@ -28,10 +30,29 @@ const BoardConfigForm = () => {
         <tr>
           <th>{t('사용_여부')}</th>
           <td>
-
+            <span>
+              <IoIosRadioButtonOff /> {t('사용')}
+            </span>
+            <span>
+              <IoIosRadioButtonOff /> {t('미사용')}
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <th>{t('한페이지_게시글_수')}</th>
+          <td>
+            <InputText type="number" name="pagePerRows" />
+          </td>
+        </tr>
+        <tr>
+          <th>{t('페이지_구간_갯수')}</th>
+          <td>
+            <InputText type="number" name="pageRanges" />
           </td>
         </tr>
       </TableCols>
+
+      
     </FormBox>
   );
 };
